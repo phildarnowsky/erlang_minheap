1bf696782ef5228ea68a0fb8ddad996bb6dbc0dc
Phil Darnowsky <phil@metamorph.(none)>
2010-08-08 17:00:50 -0400

    extract and toList implemented

--- minheap.erl
+++ minheap.erl
@@ -7,11 +7,15 @@ new(List) ->
   SortedList = lists:keysort(1, List),
   {minheap, array:from_list(SortedList)}.
 
+toList({minheap, HeapArray}) -> array:to_list(HeapArray).
+
 empty({minheap, HeapArray}) -> array:sparse_size(HeapArray) =:= 0.
 
 heap_size({minheap, HeapArray}) -> array:sparse_size(HeapArray).
 
 parentIndex(N) -> (N - 1) div 2.
+rightIndex(N) -> N * 2 + 1.
+leftIndex(N) -> N * 2 + 2.
 
 peek(Heap={minheap, HeapArray}) ->
   case empty(Heap) of
@@ -21,27 +25,96 @@ peek(Heap={minheap, HeapArray}) ->
       {item, Priority, Value}
   end.
 
+extract(Heap={minheap, HeapArray}) ->
+  TopElement = peek(Heap),
+
+  case TopElement of
+    nothing -> 
+      {nothing, Heap};
+    _ ->
+      LastIndex = heap_size(Heap) - 1,
+      LastElement = array:get(LastIndex, HeapArray),
+
+      HeapArray2 = array:set(0, LastElement, array:set(LastIndex, undefined, HeapArray)),
+      HeapArray3 = reheap(0, HeapArray2),
+      {TopElement, {minheap, HeapArray3}}
+  end.
+
 insert(Priority, Value, Heap={minheap, HeapArray}) ->
   Size = heap_size(Heap),
   HeapArray2 = array:set(Size, {Priority, Value}, HeapArray),
   HeapArray3 = bubble_up(Size, HeapArray2),
   {minheap, HeapArray3}.
 
-bubble_up(0, HeapArray) -> HeapArray;
-bubble_up(I, HeapArray) -> 
-  ParentIndex = parentIndex(I),
+swapArrayElements(I, J, Array) ->
+  Tmp = array:get(I, Array),
+  Array2 = array:set(I, array:get(J, Array), Array),
+  array:set(J, Tmp, Array2).
 
-  CurrentTuple = {CurrentPriority, _} = array:get(I, HeapArray),
-  ParentTuple = {ParentPriority, _} = array:get(ParentIndex, HeapArray),
+bubble_up(0, HeapArray) -> HeapArray;
+bubble_up(CurrentIndex, HeapArray) -> 
+  ParentIndex = parentIndex(CurrentIndex),
+
+  {CurrentPriority, _} = array:get(CurrentIndex, HeapArray),
+  {ParentPriority, _} = array:get(ParentIndex, HeapArray),
 
   if
     ParentPriority =< CurrentPriority ->
       HeapArray;
     true ->
-      HeapArray2 = array:set(ParentIndex, CurrentTuple, (array:set(I, ParentTuple, HeapArray))),
+      HeapArray2 = swapArrayElements(CurrentIndex, ParentIndex, HeapArray),
       bubble_up(ParentIndex, HeapArray2)
   end.
 
+reheap(ParentIndex, HeapArray) ->
+  RightIndex = rightIndex(ParentIndex),
+  LeftIndex = leftIndex(ParentIndex),
+
+  SmallestPriorityIndex = smallestPriorityIndex([ParentIndex, RightIndex, LeftIndex], HeapArray),
+
+  if
+    SmallestPriorityIndex =:= ParentIndex ->
+      HeapArray;
+    true ->
+      HeapArray2 = swapArrayElements(ParentIndex, SmallestPriorityIndex, HeapArray),
+      reheap(SmallestPriorityIndex, HeapArray2)
+  end.
+
+smallestPriorityIndex([InitialIndex|Indices], Array) ->
+  InitialPriority = priorityAtIndex(InitialIndex, Array),
+
+  Champion = lists:foldl(
+    fun(CurrentIndex, CurrentChampion={_SmallestIndex, SmallestPriority}) ->
+      CurrentPriority = priorityAtIndex(CurrentIndex, Array),
+      SmallerPriority = comparePriorities(SmallestPriority, CurrentPriority),
+
+      if
+        SmallerPriority =:= SmallestPriority ->
+          CurrentChampion;
+        true ->
+          {CurrentIndex, CurrentPriority}
+      end
+    end,
+
+    {InitialIndex, InitialPriority},
+    Indices
+  ),
+  
+  {SmallestIndex, _SmallestPriority} = Champion,
+  SmallestIndex.
+
+priorityAtIndex(Index, Array) ->
+  case array:get(Index, Array) of
+    {Priority, _Value} -> Priority;
+    undefined -> undefined
+  end.
+
+comparePriorities(A, undefined) -> A;
+comparePriorities(A, B) ->
+  if 
+    A > B -> B;
+    true -> A
+  end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 % TESTS
@@ -57,6 +130,13 @@ assert_has_minheap_property(Heap={minheap, HeapArray}) ->
     lists:seq(2, heap_size(Heap) - 1)
   ).
 
+  
+extract_option(OptionName, Default, PropList) ->
+  case lists:keysearch(OptionName, 1, PropList) of
+    {value, {OptionName, Value}} -> Value;
+    false -> Default
+  end.
+
 new_0_returns_empty_minheap_test() ->
   ?assert(empty(new())).
 
@@ -64,21 +144,38 @@ new_1_returns_minheap_with_equivalent_length_test() ->
   Heap = new([{3, foo}, {2, bar}, {1, baz}]),
   ?assert(heap_size(Heap) =:= 3).
 
-new_minheap_has_minheap_property_test() ->
-  Iterations = lists:seq(1, 100),
+test_on_random_heaps(TestFun) -> test_on_random_heaps(TestFun, []).
+test_on_random_heaps(TestFun, Options) ->
+  HeapCount = extract_option(heap_count, 100, Options),
+  HeapSize = extract_option(heap_size, 100, Options),
+  HeapSizeFun = extract_option(heap_size_fun, null, Options),
+  HeapElementFun = extract_option(heap_element_fun, fun random_heap_tuple/1, Options),
+
   lists:foreach(
     fun(_I) ->
-        ListLength = random:uniform(100) + 10,
-        List = lists:map(
-          fun(J) -> {random:uniform(500), J} end,
-          lists:seq(1, ListLength)
-        ),
-        Heap = new(List),
-        assert_has_minheap_property(Heap)
+      CurrentHeapSize = if 
+        HeapSizeFun /= null ->
+          HeapSizeFun();
+        true ->
+          HeapSize
+      end,
+
+      List = lists:map(
+        HeapElementFun,
+        lists:seq(1, CurrentHeapSize)
+      ),
+
+      Heap = new(List),
+      TestFun(Heap)
     end,
 
-    Iterations
+    lists:seq(1, HeapCount)
   ).
+
+random_heap_tuple(I) -> {random:uniform(500), I}.
+
+new_minheap_has_minheap_property_test() ->
+  test_on_random_heaps(fun assert_has_minheap_property/1). 
 
 peek_on_empty_minheap_returns_nothing_test() ->
   ?assert(peek(new()) =:= nothing).
@@ -99,3 +196,41 @@ insert_of_new_minimum_value_bubbles_to_top_test() ->
   NewHeap = insert(0, fnord, Heap),
   assert_has_minheap_property(NewHeap),
   ?assert(heap_size(NewHeap) =:= 5).
+
+extract_from_empty_heap_returns_nothing_test() ->
+  Heap = new(),
+  ?assert(empty(Heap)),
+  ?assert(extract(Heap) =:= {nothing, Heap}).
+
+extract_from_nonempty_heap_returns_top_element_and_new_heap_test() ->
+  test_on_random_heaps(
+    fun(Heap) ->
+      OriginalSize = heap_size(Heap),
+      {item, TopPriority, TopValue} = peek(Heap),
+
+      {{item, TopPriority, TopValue}, Heap2} = extract(Heap),
+
+      ?assert(heap_size(Heap2) =:= OriginalSize - 1),
+      assert_has_minheap_property(Heap2)
+    end
+  ).
+
+extract_from_nonempty_heap_has_all_elements_but_former_top_test() ->
+  test_on_random_heaps(
+    fun(Heap) ->
+      OriginalList = toList(Heap),
+      {{item, TopPriority, TopValue}, Heap2} = extract(Heap),
+      ExtractedList = toList(Heap2),
+
+      ListDifference = OriginalList -- ExtractedList,
+      ListDifference = [{TopPriority, TopValue}]
+    end
+  ).
+
+to_list_is_inverse_of_new_test() ->
+  test_on_random_heaps(
+    fun(Heap) ->
+      Clone = new(toList(Heap)),
+      ?assert(Heap =:= Clone)
+    end
+  ).
